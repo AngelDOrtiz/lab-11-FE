cd62b6ce33b14afe69ae208f03c717f35f2bad84
AngelDOrtiz <aortizghs@gmail.com>
2021-05-10 17:53:02 -0700

    List page complete

--- src/home/Home.js
+++ src/home/Home.js
@@ -7,9 +7,9 @@ export default class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h2>Home Page</h2>
+        <h2>The Page of Famous Cats</h2>
 
-        <Link to='/resources'>See the List</Link>
+        <Link to='/cats'>Take a look at these cats!</Link>
       </div>
     );
   }
